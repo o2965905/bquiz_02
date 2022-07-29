28ef3df72ea37681b776f9d911ffe2368aad134d
o2965905 <o2965905@gmail.com>
2022-07-29 11:15:11 +0800

    完成帳號管理的刪除帳號功能

--- api/users.php
+++ api/users.php
@@ -3,15 +3,17 @@
 <?php
 include_once "../base.php";
 
-$users=$User->all();
-foreach($users as $user){
-    echo "<tr>";
-    echo "<td>{$user['acc']}</td>";
-    echo "<td>".str_repeat("*",strlen($user['pw']))."</td>";
-    echo "<td>";
-    echo "<input type='checkbox' name='del[]' value='{$user['id']}'>";
-    echo "</td>";
-    echo "</tr>";
+$users = $User->all();
+foreach ($users as $user) {
+    if ($user['acc'] !== 'admin') {
+        echo "<tr>";
+        echo "<td>{$user['acc']}</td>";
+        echo "<td>" . str_repeat("*", strlen($user['pw'])) . "</td>";
+        echo "<td>";
+        echo "<input type='checkbox' name='del[]' value='{$user['id']}'>";
+        echo "</td>";
+        echo "</tr>";
+    }
 }
 
 
--- back/user.php
+++ back/user.php
@@ -12,13 +12,31 @@
         </tbody>
     </table>
     <div class="ct">
-        <button>確定刪除</button>
-        <button>清空選取</button>
+        <button onclick="del()">確定刪除</button>
+        <button onclick="$('table input').prop('checked',false)">清空選取</button>
     </div>
 </fieldset>
 
 <script>
-    $.get("./api/users.php",(users)=>{
+    $.get("./api/users.php", (users) => {
         $("#users").html(users)
     })
+
+    function del() {
+        let ids = new Array();
+
+        $("table input[type='checkbox']:checked").each((idx, box) => {
+            ids.push($(box).val())
+        })
+
+        // console.log(ids); //確認是否有抓到checkbox
+
+        $.post("./api/del_user.php", {
+            del: ids
+        }, () => {
+            $.get("./api/users.php", (users) => {
+                $("#users").html(users)
+            })
+        })
+    }
 </script>
