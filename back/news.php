1c0262a67bdd0d45af3ff6a3779f4d2c6387047b
o2965905 <o2965905@gmail.com>
2022-07-29 15:23:14 +0800

    完善文章管理的分頁功能

--- back/news.php
+++ back/news.php
@@ -1,6 +1,6 @@
 <!-- 最新文章管理 -->
 <form action="./api/news.php" method="post">
-<table class="tab cent ct">
+<table class="ct" style="width:80%;margin:auto">
     <tr>
         <th width="10%">編號</th>
         <th width="70%">標題</th>
@@ -30,7 +30,33 @@
     }
     ?>
 </table>
+<div class="ct">
+    <!-- 顯示分頁按鈕 -->
+    <?php
 
+        if(($now-1)>0){ //上一頁的符號『<』
+            $prev=$now-1;
+            echo "<a href='?do=news&p=$prev'>";
+            echo " < ";
+            echo "</a>";
+        }
+
+        for($i=1;$i<=$pages;$i++){ //分頁中間的數字 『1 2 3』
+            $fontsize=($now==$i)?'24px':'16px';
+            echo "<a href='?do=news&p={$i}' style='font-size:$fontsize'> ";
+            echo $i;
+            echo " </a>";
+        }
+
+        if(($now+1)<=$pages){ //下一頁的符號『>』
+            $next=$now+1;
+            echo "<a href='?do=news&p=$next'>";
+            echo " > ";
+            echo "</a>";
+        }
+
+    ?>
+</div>
 <div class="ct"><input type="submit" value="確定修改"></div>
 
 </form>
