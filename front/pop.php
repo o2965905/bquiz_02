8c6c76b1674365513542839a396865db31e94854
o2965905 <o2965905@gmail.com>
2022-08-01 11:16:20 +0800

    完成最新文章顯示及分頁功能

---
+++ front/pop.php
@@ -0,0 +1,12 @@
+<!-- 前台_人氣文章區 -->
+<fieldset>
+    <legend>目前位置 : 首頁 > 人氣文章區</legend>
+    <table>
+        <tr>
+            <td width="30%">標題</td>
+            <td width="50%">內容</td>
+            <td width="20%">人氣</td>
+        </tr>
+    </table>
+</fieldset>
+
